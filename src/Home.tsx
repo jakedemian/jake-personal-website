b1f0b5dd1588e933e6105459bbc68b14fd41e1ed
Jake Demian <jakedemian@gmail.com>
2023-05-09 22:55:24 -0400

    init commit

---
+++ src/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <div style={{ backgroundColor: "#000", flex: 1, height: "100vh" }}>
+        <p style={{ color: "white" }}>it is an app</p>
+      </div>
+    </>
+  );
+};
+
+export default Home;
