7776a18427a81ee209f3494e3c91665d676d90b3
Jake Demian <jakedemian@gmail.com>
2023-05-28 20:36:27 -0400

    Layout fixes, placeholder pages (#9)

--- src/Routes/Experience/index.tsx
+++ src/Routes/Experience/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Text } from '@chakra-ui/react';
+import { Image, Text, VStack } from '@chakra-ui/react';
 import React from 'react';
 
 import Page from 'src/Routes/Page';
@@ -6,9 +6,18 @@ import Page from 'src/Routes/Page';
 const Experience: React.FC = () => {
   return (
     <Page title="Experience">
-      <Box>
-        <Text>Hi</Text>
-      </Box>
+      <VStack alignItems={{ base: 'center', lg: 'flex-start' }}>
+        <VStack mt={8}>
+          <Image
+            color="dark.text"
+            brightness={255}
+            src="/img/bruce-almighty-jim-carrey.gif"
+            w="65%"
+            borderRadius={8}
+          />
+          <Text fontSize={24}>I&apos;m working on it!</Text>
+        </VStack>
+      </VStack>{' '}
     </Page>
   );
 };
