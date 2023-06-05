cd3acd6b5954c1caa71747721e66dab4dfede1de
Jake Demian <jakedemian@gmail.com>
2023-06-05 15:59:49 -0400

    contact page mobile styles

--- src/Routes/Contact/Email.tsx
+++ src/Routes/Contact/Email.tsx
@@ -7,6 +7,8 @@ import {
   Text,
   Textarea,
   Tooltip,
+  useTheme,
+  VStack,
 } from '@chakra-ui/react';
 import React, { useState } from 'react';
 import { FaCheck, FaQuestionCircle } from 'react-icons/fa';
@@ -30,7 +32,8 @@ type FormError = {
 const Email: React.FC = () => {
   const [formState, setFormState] = useState<FormState>(defaultFormState);
   const [error, setError] = useState<FormError | null>(null);
-  const [complete /* setComplete */] = useState<boolean>(false);
+  const [complete, setComplete] = useState<boolean>(false);
+  const { colors } = useTheme();
 
   const FormErrorMessage = ({ children }: { children: string }) => {
     return (
@@ -40,68 +43,67 @@ const Email: React.FC = () => {
     );
   };
 
-  // TODO uncomment handleSubmit below, killing prod emails for now
-  // const handleSubmit = async () => {
-  //   if (!formState.email) {
-  //     setError({
-  //       field: 'email',
-  //       errorMessage: 'Email address is required so that I can respond to you!',
-  //     });
-  //   }
+  const handleSubmit = async () => {
+    if (!formState.email) {
+      setError({
+        field: 'email',
+        errorMessage: 'Email address is required so that I can respond to you!',
+      });
+    }
 
-  //   if (
-  //     // if this regex doesn't work you can blame chat gpt (I even used v4 with the browsing plugin!)
-  //     !formState.email.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)
-  //   ) {
-  //     setError({
-  //       field: 'email',
-  //       errorMessage: 'A valid email address must be used.',
-  //     });
-  //   }
+    if (
+      // if this regex doesn't work you can blame chat gpt (I even used v4 with the browsing plugin!)
+      !formState.email.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)
+    ) {
+      setError({
+        field: 'email',
+        errorMessage: 'A valid email address must be used.',
+      });
+    }
 
-  //   if (!formState.message) {
-  //     setError({
-  //       field: 'message',
-  //       errorMessage: 'A message is required',
-  //     });
-  //   }
+    if (!formState.message) {
+      setError({
+        field: 'message',
+        errorMessage: 'A message is required',
+      });
+    }
 
-  //   // TODO this only works in production at the moment, so I need
-  //   //to a) create a local vercel dev environment for testing this,
-  //   //I also need to mock the email sending, and finally I need to
-  //   //wrap this is something like react query instead of using a raw
-  //   //fetch
-  //   const response = await fetch('https://jakedemian.dev/api/send-email', {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //     body: JSON.stringify({
-  //       to: 'jakedemian@gmail.com',
-  //       from: 'jakedemian@gmail.com',
-  //       subject: '👀 jakedemian.dev email form',
-  //       text: `From <${formState.email}>: ${formState.message}`,
-  //     }),
-  //   });
+    // TODO this only works in production at the moment, so I need
+    //to a) create a local vercel dev environment for testing this,
+    //I also need to mock the email sending, and finally I need to
+    //wrap this is something like react query instead of using a raw
+    //fetch
+    const response = await fetch('https://jakedemian.dev/api/send-email', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        to: 'jakedemian@gmail.com',
+        from: 'jakedemian@gmail.com',
+        subject: '👀 jakedemian.dev email form',
+        text: `From <${formState.email}>: ${formState.message}`,
+      }),
+    });
 
-  //   if (response.status === 200) {
-  //     setComplete(true);
-  //   }
-  // };
+    if (response.status === 200) {
+      setComplete(true);
+    }
+  };
 
   if (complete) {
     return (
-      <HStack>
-        <Text>
+      <VStack p={{ base: 8, lg: 0 }} textAlign={'center'} mt={8}>
+        <Text fontSize={20}>
           Success! Thanks for reaching out, I&apos;ll get back to you soon!
         </Text>
-        <Icon as={FaCheck} color="green.500" />
-      </HStack>
+        <FaCheck size={70} color={colors.green[500]} />
+      </VStack>
     );
   }
 
   return (
-    <>
+    <VStack w="100%" alignItems="left" p={{ base: 8, lg: 0 }}>
       <HStack alignItems="center">
         <Text>Email Address</Text>
         <Tooltip
@@ -149,16 +151,15 @@ const Email: React.FC = () => {
         bg="primary.500"
         rightIcon={<Icon as={HiOutlineMail} />}
         _disabled={{ bg: '#1b1b1b', color: '#555' }}
-        isDisabled={true}
-
-        // TODO uncomment onClick below, killing prod emails for now
-        // isDisabled={!formState.email || !formState.message}
-        // onClick={() => handleSubmit()}
+        isDisabled={!formState.email || !formState.message}
+        onClick={() => handleSubmit()}
+        // TODO this is stretching automatically without setting a width due to
+        //flex and i'm so close to release I'll fix it later, hardcoding for now
+        w={{ base: 'auto', lg: 150 }}
       >
         Send
       </Button>
-      <Text>Emails temporarily disabled</Text>
-    </>
+    </VStack>
   );
 };
 
