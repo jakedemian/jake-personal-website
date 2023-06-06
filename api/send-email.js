8035653be7aa26504873beaa1a6790a94524c39e
Jake Demian <jakedemian@gmail.com>
2023-06-06 00:34:58 -0400

    Vercel local dev env, mock sendemail function (#22)

---
+++ api/send-email.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-undef */
+require('@sendgrid/mail');
+
+let sgMail;
+if (process.env.NODE_ENV === 'production') {
+  sgMail = require('@sendgrid/mail');
+} else {
+  sgMail = require('../mocks/sendgrid');
+}
+
+module.exports = async (req, res) => {
+  try {
+    const sendgridApiKey = process.env.SENDGRID_API_KEY;
+    if (!sendgridApiKey) {
+      throw new Error('SENDGRID_API_KEY was not found.');
+    }
+
+    sgMail.setApiKey(sendgridApiKey);
+
+    const { to, from, subject, text } = req.body;
+
+    const msg = {
+      to,
+      from,
+      subject,
+      text,
+    };
+
+    await sgMail.send(msg);
+    res.status(200).send('Email sent successfully');
+  } catch (error) {
+    console.error(error);
+    if (error.response) {
+      console.error(error.response.body);
+    }
+    res.status(500).send(`Error sending email: ${JSON.stringify(error)}`);
+  }
+};
---
+++ mocks/sendgrid.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+module.exports = {
+  setApiKey: () => {},
+  send: async () => {
+    console.log('Mock Sendgrid: Email sent successfully');
+  },
+};
