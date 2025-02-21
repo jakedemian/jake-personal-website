6de27a83ece217c367611416ad7500f498e2bfbe
Jake Demian <jakedemian@gmail.com>
2025-02-21 15:18:02 -0500

    get count update

--- api/getCount.js
+++ api/getCount.js
@@ -1,13 +1,22 @@
 /* eslint-disable no-undef */
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { createClient } = require('@vercel/kv');
+import { NextResponse } from 'next/server';
+import { get } from '@vercel/edge-config';
 
 module.exports = async (req, res) => {
-  const clicks = createClient({
-    url: process.env.KV_REST_API_URL,
-    token: process.env.KV_REST_API_TOKEN,
-  });
-
-  const clickCount = await clicks.hgetall('clickCount');
-  return res.status(200).json({ clickCount });
+  const greeting = await get('clickCount');
+  // NextResponse.json requires at least Next v13.1 or
+  // enabling experimental.allowMiddlewareResponseBody in next.config.js
+  console.log(greeting);
+  return NextResponse.json(greeting);
 };
+
+// module.exports = async (req, res) => {
+//   const clicks = createClient({
+//     url: process.env.KV_REST_API_URL,
+//     token: process.env.KV_REST_API_TOKEN,
+//   });
+
+//   const clickCount = await clicks.hgetall('clickCount');
+//   return res.status(200).json({ clickCount });
+// };
