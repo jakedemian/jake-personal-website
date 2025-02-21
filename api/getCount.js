abc75392a99f102f5560a0022292298669d9f98e
Jake Demian <jakedemian@gmail.com>
2025-02-21 16:20:40 -0500

    edgedb attempt

--- api/getCount.js
+++ api/getCount.js
@@ -1,7 +1,13 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-undef */
 /* eslint-disable @typescript-eslint/no-var-requires */
+const edgedb = require('edgedb');
 
 module.exports = async (req, res) => {
-  return 1000;
+  const client = edgedb.createClient();
+  console.log('🏀', client);
+
+  const result = await client.query(`select 2 + 2;`);
+  console.log(result); // [4]
+  return res.status(200).json({ clickCount: { value: 0 } });
 };
