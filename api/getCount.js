166286c3004ec8e4b58b1ae132a18d873bfc6326
Jake Demian <jakedemian@gmail.com>
2025-03-03 21:39:24 -0500

    silencing an eslint error in a javascript file

--- api/getCount.js
+++ api/getCount.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-undef */
 /* eslint-disable @typescript-eslint/no-var-requires */
 import { NextResponse } from 'next/server';
